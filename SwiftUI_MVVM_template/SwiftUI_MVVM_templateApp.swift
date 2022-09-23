6b95422201d0239e100984272353f85abe6cc523
Leopold Romanowski <leopold.romanowski@escolasoft.com>
2022-09-23 13:26:32 +0200

    Initial Commit

---
+++ SwiftUI_MVVM_template/SwiftUI_MVVM_templateApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_MVVM_templateApp.swift
+//  SwiftUI_MVVM_template
+//
+//  Created by Leopold Romanowski on 23/09/2022.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_MVVM_templateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
